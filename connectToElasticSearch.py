26c8827ad7664eb9f3d1a762448eee52775edf5d
piyush <adhikaripiyush41@gmail.com>
2021-12-05 19:02:04 -0800

    Successfully connected elasticsearch index via python.

--- connectToElasticSearch.py
+++ connectToElasticSearch.py
@@ -1,1 +1,18 @@
 #!/bin/python3
+
+# Connecting python with the elasticsearch
+
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+
+client = Elasticsearch('http://localhost:9200')
+
+
+s = Search(index='authlog_index').using(client)
+
+response = s.execute()
+
+for hit in response:
+    print(hit.meta.score, hit.message)
+
+
