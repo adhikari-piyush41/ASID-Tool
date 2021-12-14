825d66a0e040343ada7ea8358d3c38de27103729
piyush <adhikaripiyush41@gmail.com>
2021-12-13 22:38:45 -0800

    Queried for the authentication failure contents.

--- connectToElasticSearch.py
+++ connectToElasticSearch.py
@@ -3,16 +3,31 @@
 # Connecting python with the elasticsearch
 
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Q
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import MultiMatch, Match, QueryString
 
-client = Elasticsearch('http://localhost:9200')
+client = Elasticsearch('http://localhost:9200', size=10000)
 
+# Check if an elasticsearch is running or not
+if not client.ping():
+    raise ValueError("Connection failed")
+else:
+    print ("-------------------------------------------------")
+    print ("Connection started!!!")
+    print ("-------------------------------------------------")
+s = Search(index='authorization_index').using(client)
+'''"match": {
+            "message": {
+              "query": "SSH"
+            }'''
+queries = Match(message={"query": "sshd:auth"})
 
-s = Search(index='authlog_index').using(client)
-
-response = s.execute()
+s = s.query(queries)
+response = s.scan()
 
 for hit in response:
-    print(hit.meta.score, hit.message)
+    print (hit.message)
 
 
+#queries = QueryString(query={"message": "(sshd) OR (vsftpd)"})
+#print('Total hits found is', response.hits.total)
